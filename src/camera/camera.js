88f97f8abceae9ec00b2ed5ff71574eddca3b889
kiri10ten <126086612+kiri10ten@users.noreply.github.com>
2023-12-24 12:46:25 +0530

    added camera module

---
+++ src/camera/camera.js
@@ -0,0 +1,7 @@
+export default class Camera{
+
+    constructor ()
+    {
+        console.log(" Hey i am camera ")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import Camera from "./camera/camera";
+
 console.log("Hello from index.js!");
+
+const camera = new Camera()
