18c231a3345d6b5713e103359663dd6f7fb10307
kiri10ten <126086612+kiri10ten@users.noreply.github.com>
2023-12-25 18:20:45 +0530

    instance

--- src/camera/camera.js
+++ src/camera/camera.js
@@ -1,7 +1,19 @@
+import * as THREE from "three"
+
+
 export default class Camera{
 
     constructor ()
     {
-        console.log(" Hey i am camera ")
+        this.instance = new THREE.PerspectiveCamera(
+            35,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            100);
+        
+        this.instance.position.z = 5;
+
+
+      
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Camera from "./camera/camera";
 
+import * as THREE from 'three'
+
 console.log("Hello from index.js!");
 
 const camera = new Camera()
+console.log(camera.instance)
